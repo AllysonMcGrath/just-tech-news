925e169005a29adbfc7c1344b7367e43696553ad
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2021-11-04 14:19:04 -0500

    add sessions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+
 router.get('/', (req, res) => {
+  console.log(req.session);
     Post.findAll({
       attributes: [
         'id',
@@ -35,6 +37,10 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+  });
+
+  router.get('/login', (req, res) => {
+    res.render('login');
   });
 
 module.exports = router;
